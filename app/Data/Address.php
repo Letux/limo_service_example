7d53c91b4313a339d6c6cb4fb17f325d5b79ac32
Alexey Russkin <alexey.russkin@gmail.com>
2023-11-15 16:04:43 +0100

    Added test for Address data class

--- app/Data/Address.php
+++ app/Data/Address.php
@@ -8,4 +8,41 @@ class Address
     {
 
     }
+
+    public static function createFromString(string $address): Address
+    {
+        $address = self::cleanAddressString($address);
+
+        $result = preg_match('/^([\w\s]+)\s*,\s*(\w{2}) (\d{5})$/', $address, $matches);
+
+        if ($result === 1) {
+            return new Address(trim($matches[1]), $matches[2], $matches[3]);
+        }
+
+        $result = preg_match('/^([\w\s]+)\s*,\s*(\w{2})$/', $address, $matches);
+
+        if ($result === 1) {
+            return new Address(trim($matches[1]), $matches[2], null);
+        }
+
+        $result = preg_match('/^([\w\s]+)\s*,$/', $address, $matches);
+
+        if ($result === 1) {
+            return new Address(trim($matches[1]), null, null);
+        }
+
+        $result = preg_match('/^([\w\s]+)$/', $address, $matches);
+
+        if ($result === 1) {
+            return new Address(trim($matches[1]), null, null);
+        }
+
+        return new Address(null, null, null);
+    }
+
+    public static function cleanAddressString(string $address): string
+    {
+        $address = trim($address);
+        return preg_replace('/\s+/', ' ', $address);
+    }
 }
---
+++ tests/Unit/Data/AddressTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Unit\Data;
+
+use App\Data\Address;
+use PHPUnit\Framework\TestCase;
+
+final class AddressTest extends TestCase
+{
+    public function testCreateFromString()
+    {
+        $address = Address::createFromString('New    York  ,    NY   10001 ');
+        $this->assertEquals('New York', $address->city);
+        $this->assertEquals('NY', $address->state);
+        $this->assertEquals('10001', $address->zip);
+
+        $address = Address::createFromString('New York, NY');
+        $this->assertEquals('New York', $address->city);
+        $this->assertEquals('NY', $address->state);
+        $this->assertNull($address->zip);
+
+        $address = Address::createFromString('New York, ');
+        $this->assertEquals('New York', $address->city);
+        $this->assertNull($address->state);
+        $this->assertNull($address->zip);
+
+        $address = Address::createFromString('New York');
+        $this->assertEquals('New York', $address->city);
+        $this->assertNull($address->state);
+        $this->assertNull($address->zip);
+    }
+
+    public function testCleanAddressString()
+    {
+        $address = Address::cleanAddressString(' New    York  ,    NY   10001 ');
+        $this->assertEquals('New York , NY 10001', $address);
+    }
+}
